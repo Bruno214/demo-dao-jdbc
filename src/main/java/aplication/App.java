205992afff366ff1b0470db792190ee8589f1e00
Bruno214 <bruno214215216217218@gmail.com>
2023-04-19 14:26:25 -0300

    created SellerDaoJDBC and DaoFactory

--- src/main/java/aplication/App.java
+++ src/main/java/aplication/App.java
@@ -1,6 +1,8 @@
 package aplication;
 
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 import java.util.Date;
@@ -10,7 +12,9 @@ public class App {
 
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000, obj);
-        System.out.println(seller);
 
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println(seller);
     }
 }
